bb12da00667973e81de9812dc57a07e39b98f8e3
shravanvele <shravanvele942@gmail.com>
2026-01-20 15:18:01 +0530

    Add files via upload

---
+++ CodeAlpha_Social_media_Platform/models/Post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [
+    {
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      text: String,
+      createdAt: { type: Date, default: Date.now }
+    }
+  ]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
